157e0e3b59e57d34b6d3c134ae2406c3f2666149
switchtft <andrebalao26@gmail.com>
2025-08-09 21:37:42 +0100

    maybe

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { initSocketServer } from './src/lib/socket';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
 
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
