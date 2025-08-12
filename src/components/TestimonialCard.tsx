3e6f0acfe0c474ce3d10834d001a0a025688c2c1
switchtft <andrebalao26@gmail.com>
2025-08-12 21:58:45 +0100

    again!

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -7,7 +7,7 @@ export default function TestimonialCard({
 }) {
   return (
     <div className="bg-[#1f1f1f] p-6 rounded-xl max-w-sm mx-auto">
-      <p className="italic text-[#e6915b]/80 mb-4">"{text}"</p>
+      <p className="italic text-[#e6915b]/80 mb-4">&quot;{text}&quot;</p>
       <span className="text-[#d4a574] font-semibold">{name}</span>
     </div>
   );
